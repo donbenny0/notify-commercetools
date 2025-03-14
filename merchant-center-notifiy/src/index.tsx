6694e3d586a06b477ba67cad089a3a8f6f796859
Don Benny <don.benny@ayatacommerce.com>
2025-03-14 16:53:02 +0530

    Update postDeploy script and enhance environment variable logging in index.tsx

--- merchant-center-notifiy/src/index.tsx
+++ merchant-center-notifiy/src/index.tsx
@@ -6,7 +6,12 @@ import EntryPoint from './components/entry-point';
 console.log('🚀 - Starting the app');
 
 // print all environmet variables
-console.log('🚀 - process.env', process.env);
-
+console.log('🚀 - process.env.CLOUD_IDENTIFIER', process.env.CLOUD_IDENTIFIER);
+console.log('🚀 - process.env.PROJECT_ID', process.env.PROJECT_ID);
+console.log('🚀 - process.env.CUSTOM_APPLICATION_ID', process.env.CUSTOM_APPLICATION_ID);
+console.log('🚀 - process.env.ENABLE_NEW_JSX_TRANSFORM', process.env.ENABLE_NEW_JSX_TRANSFORM);
+console.log('🚀 - process.env.FAST_REFRESH', process.env.FAST_REFRESH);
+console.log('🚀 - process.env.ENTRY_POINT_URI_PATH', process.env.ENTRY_POINT_URI_PATH);
+console.log('🚀 - process.env.APPLICATION_URL', process.env.APPLICATION_URL);
 
 ReactDOM.render(<EntryPoint />, document.getElementById('app'));
